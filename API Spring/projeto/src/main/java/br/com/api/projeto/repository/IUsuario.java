bd7edaff0fcf30c94efdc12935ee7e42bc67d108
Igor Suzuki Kira <89170220+igorsuzuki99@users.noreply.github.com>
2022-09-18 13:52:59 -0300

    Delete API Spring/projeto directory

--- API Spring/projeto/src/main/java/br/com/api/projeto/repository/IUsuario.java
+++
@@ -1,8 +0,0 @@
-package br.com.api.projeto.repository;
-
-import br.com.api.projeto.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IUsuario extends JpaRepository<Usuario, Integer> {
-
-}
