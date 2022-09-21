9aaf9cfd4f92f46df36704bfa1bc286c021a60b8
Igor Suzuki Kira <igorsuzukiprof@gmail.com>
2022-09-21 10:19:15 -0300

    Atualizando

--- Teste API Spring/projeto/src/main/java/br/com/api/testeProjeto/repository/IRegiao.java
+++
@@ -1,7 +0,0 @@
-package br.com.api.testeProjeto.repository;
-
-import br.com.api.testeProjeto.model.Regiao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IRegiao extends JpaRepository<Regiao, Integer> {
-}
